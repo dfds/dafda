b3b4f3ae4f8462ec1366c20afc84f6374b601f3a
Jens Andresen <jandr@DFDS.COM>
2019-08-15 10:07:06 +0200

    add dummy test double for creating dummies in tests

---
+++ src/Dafda.Tests/TestDoubles/Dummy.cs
@@ -0,0 +1,12 @@
+﻿using Moq;
+
+namespace Dafda.Tests.TestDoubles
+{
+    public static class Dummy
+    {
+        public static T Of<T>() where T : class
+        {
+            return new Mock<T>().Object;
+        }
+    }
+}
