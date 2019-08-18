f2b8206316c63e99093cf736725e8bea185f45a8
Thomas Fischer <thfis@dfds.com>
2019-08-18 22:47:29 +0200

    move hosted service into Consuming namespace

---
+++ src/Dafda.AspNetCore/Consuming/SubscriberHostedService.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Dafda.Consuming
+{
+    internal class SubscriberHostedService : BackgroundService
+    {
+        private readonly ILogger<SubscriberHostedService> _logger;
+        private readonly IApplicationLifetime _applicationLifetime;
+        private readonly TopicSubscriber _topicSubscriber;
+
+        public SubscriberHostedService(ILogger<SubscriberHostedService> logger, IApplicationLifetime applicationLifetime, TopicSubscriber topicSubscriber)
+        {
+            _logger = logger;
+            _applicationLifetime = applicationLifetime;
+            _topicSubscriber = topicSubscriber;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            return Task.Run(async () =>
+            {
+                try
+                {
+                    _logger.LogDebug("SubscriberHostedService started");
+                    await _topicSubscriber.Start(stoppingToken);
+                }
+                catch (OperationCanceledException)
+                {
+                    _logger.LogDebug("SubscriberHostedService cancelled");
+                }
+                catch (Exception err)
+                {
+                    _logger.LogError(err, "Unhandled error occurred while consuming messaging");
+                    _applicationLifetime.StopApplication();
+                }
+            }, stoppingToken);
+        }
+    }
+}
