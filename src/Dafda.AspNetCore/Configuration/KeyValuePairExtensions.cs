1367bea4f36a92551ba69d43f3c24496cb60eb37
Thomas Fischer <thfis@dfds.com>
2019-08-18 22:07:40 +0200

    add test to KeyValuePairExtensions

--- src/Dafda.AspNetCore/Configuration/KeyValuePairExtensions.cs
+++ src/Dafda.AspNetCore/Configuration/KeyValuePairExtensions.cs
@@ -1,13 +1,8 @@
-using System;
 using System.Collections.Generic;
-using Confluent.Kafka;
-using Dafda.Messaging;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace Dafda.Configuration
 {
-    public static class Extensions
+    internal static class KeyValuePairExtensions
     {
         public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> keyValuePair, out TKey key, out TValue value)
         {
