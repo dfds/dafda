19cb6a331d576f3ba2189e438e3851bf3ff9fccd
Thomas Fischer <thfis@dfds.com>
2019-09-16 16:22:35 +0200

    add logging to KafkaConsumerScope

--- src/Dafda/Consuming/KafkaConsumerScope.cs
+++ src/Dafda/Consuming/KafkaConsumerScope.cs
@@ -1,12 +1,15 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Confluent.Kafka;
+using Dafda.Logging;
 using Dafda.Messaging;
 
 namespace Dafda.Consuming
 {
     internal class KafkaConsumerScope : TopicSubscriberScope
     {
+        private static readonly ILog Log = LogProvider.GetCurrentClassLogger();
+
         private readonly IConsumer<string, string> _innerKafkaConsumer;
 
         internal KafkaConsumerScope(IConsumer<string, string> innerKafkaConsumer)
@@ -17,6 +20,8 @@ namespace Dafda.Consuming
         public override Task<MessageResult> GetNext(CancellationToken cancellationToken)
         {
             var innerResult = _innerKafkaConsumer.Consume(cancellationToken);
+
+            Log.Debug("Received message {Key}: {RawMessage}", innerResult.Key, innerResult.Value);
 
             var result = new MessageResult(
                 message: new JsonMessageEmbeddedDocument(innerResult.Value),
