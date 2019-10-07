ab8316c6232075614ed488d3232afec85b6a9ff8
Thomas Fischer <thfis@dfds.com>
2019-10-07 18:14:54 +0200

    move interface next to implementation

--- src/Dafda.AspNetCore/Configuration/ConsumerOptions.cs
+++ src/Dafda.AspNetCore/Configuration/ConsumerOptions.cs
@@ -5,6 +5,26 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Dafda.Configuration
 {
+    public interface IConsumerOptions
+    {
+        void WithConfigurationSource(ConfigurationSource configurationSource);
+        void WithConfigurationSource(Microsoft.Extensions.Configuration.IConfiguration configuration);
+        void WithNamingConvention(NamingConvention namingConvention);
+        void WithEnvironmentStyle(string prefix = null, params string[] additionalPrefixes);
+        void WithConfiguration(string key, string value);
+        void WithGroupId(string groupId);
+        void WithBootstrapServers(string bootstrapServers);
+        void WithUnitOfWorkFactory<T>() where T : class, IHandlerUnitOfWorkFactory;
+        void WithUnitOfWorkFactory(Func<IServiceProvider, IHandlerUnitOfWorkFactory> implementationFactory);
+        void WithUnitOfWork<T>() where T : ScopedUnitOfWork;
+        void WithUnitOfWork(Func<IServiceProvider, ScopedUnitOfWork> implementationFactory);
+        void WithTopicSubscriberScopeFactory(ITopicSubscriberScopeFactory topicSubscriberScopeFactory);
+
+        void RegisterMessageHandler<TMessage, TMessageHandler>(string topic, string messageType)
+            where TMessage : class, new()
+            where TMessageHandler : class, IMessageHandler<TMessage>;
+    }
+
     internal class ConsumerOptions : IConsumerOptions
     {
         private readonly ConsumerConfigurationBuilder _builder;
--- src/Dafda.AspNetCore/Configuration/ProducerOptions.cs
+++ src/Dafda.AspNetCore/Configuration/ProducerOptions.cs
@@ -1,5 +1,15 @@
 namespace Dafda.Configuration
 {
+    public interface IProducerOptions
+    {
+        void WithConfigurationSource(ConfigurationSource configurationSource);
+        void WithConfigurationSource(Microsoft.Extensions.Configuration.IConfiguration configuration);
+        void WithNamingConvention(NamingConvention namingConvention);
+        void WithEnvironmentStyle(string prefix = null, params string[] additionalPrefixes);
+        void WithConfiguration(string key, string value);
+        void WithBootstrapServers(string bootstrapServers);
+    }
+
     internal class ProducerOptions : IProducerOptions
     {
         private readonly ProducerConfigurationBuilder _builder;
