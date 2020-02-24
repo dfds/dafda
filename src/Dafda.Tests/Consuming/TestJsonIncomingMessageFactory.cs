3a09a48ad2b13cc3a82ea1784848268c9f880e34
Thomas Fischer <thfis@dfds.com>
2020-02-24 15:28:21 +0100

    clean up tests

--- src/Dafda.Tests/Consuming/TestJsonIncomingMessageFactory.cs
+++ src/Dafda.Tests/Consuming/TestJsonIncomingMessageFactory.cs
@@ -6,7 +6,7 @@ namespace Dafda.Tests.Consuming
 {
     public class TestJsonIncomingMessageFactory
     {
-        private const string MessageJson = "{\"messageId\":\"1\",\"type\":\"vehicle_position_changed\",\"data\":{\"aggregateId\":\"1\",\"messageId\":\"1\",\"vehicleId\":\"1\",\"timeStamp\":\"2019-09-16T07:59:01Z\",\"position\":{\"latitude\":1,\"longitude\":2}}}";
+        private const string MessageJson = "{\"messageId\":\"message-id\",\"type\":\"vehicle_position_changed\",\"data\":{\"aggregateId\":\"aggregate-id\",\"vehicleId\":\"vehicle-id\",\"timeStamp\":\"2019-09-16T07:59:01Z\",\"position\":{\"latitude\":1,\"longitude\":2}}}";
 
         [Fact]
         public void Can_access_message_headers()
@@ -15,7 +15,7 @@ namespace Dafda.Tests.Consuming
 
             var message = sut.Create(MessageJson);
 
-            Assert.Equal("1", message.Metadata.MessageId);
+            Assert.Equal("message-id", message.Metadata.MessageId);
             Assert.Equal("vehicle_position_changed", message.Metadata.Type);
         }
 
@@ -27,9 +27,8 @@ namespace Dafda.Tests.Consuming
             var message = sut.Create(MessageJson);
             var data = (VehiclePositionChanged) message.ReadDataAs(typeof(VehiclePositionChanged));
 
-            Assert.Equal("1", data.AggregateId);
-            Assert.Equal("1", data.MessageId);
-            Assert.Equal("1", data.VehicleId);
+            Assert.Equal("aggregate-id", data.AggregateId);
+            Assert.Equal("vehicle-id", data.VehicleId);
             Assert.Equal(new DateTime(2019, 9, 16, 7, 59, 1, DateTimeKind.Utc), data.TimeStamp);
             Assert.Equal(1, data.Position.Latitude);
             Assert.Equal(2, data.Position.Longitude);
@@ -38,7 +37,6 @@ namespace Dafda.Tests.Consuming
         public class VehiclePositionChanged
         {
             public string AggregateId { get; set; }
-            public string MessageId { get; set; }
             public string VehicleId { get; set; }
             public DateTime TimeStamp { get; set; }
             public Position Position { get; set; }
