789ff2000ce249c04701d1a132d0ae1d25ff58a1
Thomas Fischer <thfis@dfds.com>
2020-02-28 16:21:38 +0100

    rename configuration extensions class

--- src/Dafda/Configuration/OutboxServiceCollectionExtensions.cs
+++ src/Dafda/Configuration/OutboxServiceCollectionExtensions.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace Dafda.Configuration
 {
-    public static class OutboxProducerServiceCollectionExtensions
+    public static class OutboxServiceCollectionExtensions
     {
         public static void AddOutbox(this IServiceCollection services, Action<OutboxProducerOptions> options)
         {
