bc3f30845d622d2637c9a5b55aad3174ceaaf4b1
Thomas Fischer <thfis@dfds.com>
2020-02-29 19:15:30 +0100

    protect against calling disposed object
    thread crashes and deadlocks otherwise

--- src/Dafda/Outbox/OutboxNotification.cs
+++ src/Dafda/Outbox/OutboxNotification.cs
@@ -8,24 +8,42 @@ namespace Dafda.Outbox
         private readonly AutoResetEvent _autoResetEvent = new AutoResetEvent(false);
         private readonly TimeSpan _delay;
 
+        private bool _disposed;
+
         public OutboxNotification(TimeSpan delay)
         {
             _delay = delay;
         }
-        
+
         public void Notify()
         {
+            if (_disposed)
+            {
+                return;
+            }
+
             _autoResetEvent.Set();
         }
 
         public void Wait()
         {
+            if (_disposed)
+            {
+                return;
+            }
+
             _autoResetEvent.WaitOne(_delay);
         }
 
         public void Dispose()
         {
+            if (_disposed)
+            {
+                return;
+            }
+
             _autoResetEvent.Dispose();
+            _disposed = true;
         }
     }
 }
