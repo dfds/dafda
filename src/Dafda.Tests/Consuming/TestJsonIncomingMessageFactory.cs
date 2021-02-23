21778f1e13d573e4fa9547a49a6e65a202527933
cristian-dfds <79208872+cristian-dfds@users.noreply.github.com>
2021-02-23 10:48:46 +0100

    Fix test for case insensitive fields in data object

--- src/Dafda.Tests/Consuming/TestJsonIncomingMessageFactory.cs
+++ src/Dafda.Tests/Consuming/TestJsonIncomingMessageFactory.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Dafda.Consuming;
 using Xunit;
 
@@ -30,19 +30,6 @@ namespace Dafda.Tests.Consuming
             
             Assert.Equal("12345", message.Metadata.MessageId);
         }
-        
-        private const string MessageJsonWithNonCamelCaseFields = "{\"MessageId\":12345, \"tYPe\":\"vehicle_position_changed\",\"data\":{\"aggregateId\":\"aggregate-id\",\"vehicleId\":\"vehicle-id\",\"timeStamp\":\"2019-09-16T07:59:01Z\",\"position\":{\"latitude\":1,\"longitude\":2}}}";
-
-        [Fact]
-        public void Can_read_message_headers_non_string()
-        {
-            var sut = new JsonIncomingMessageFactory();
-
-            var message = sut.Create(MessageJsonWithNonCamelCaseFields);
-
-            Assert.Equal("message-id", message.Metadata.MessageId);
-            Assert.Equal("vehicle_position_changed", message.Metadata.Type);
-        }
 
         [Fact]
         public void Can_decode_data_body()
@@ -51,6 +38,23 @@ namespace Dafda.Tests.Consuming
             
             var message = sut.Create(MessageJson);
             var data = (VehiclePositionChanged) message.ReadDataAs(typeof(VehiclePositionChanged));
+
+            Assert.Equal("aggregate-id", data.AggregateId);
+            Assert.Equal("vehicle-id", data.VehicleId);
+            Assert.Equal(new DateTime(2019, 9, 16, 7, 59, 1, DateTimeKind.Utc), data.TimeStamp);
+            Assert.Equal(1, data.Position.Latitude);
+            Assert.Equal(2, data.Position.Longitude);
+        }
+
+        private const string MessageJsonWithNonCamelCaseFields = "{\"messageId\":12345, \"type\":\"vehicle_position_changed\",\"data\":{\"aggregateId\":\"aggregate-id\",\"VehicleId\":\"vehicle-id\",\"TIMEStamp\":\"2019-09-16T07:59:01Z\",\"PosiTion\":{\"latitude\":1,\"longitude\":2}}}";
+
+        [Fact]
+        public void Can_read_message_with_non_camel_case_data_fields()
+        {
+            var sut = new JsonIncomingMessageFactory();
+
+            var message = sut.Create(MessageJsonWithNonCamelCaseFields);
+            var data = (VehiclePositionChanged)message.ReadDataAs(typeof(VehiclePositionChanged));
 
             Assert.Equal("aggregate-id", data.AggregateId);
             Assert.Equal("vehicle-id", data.VehicleId);
