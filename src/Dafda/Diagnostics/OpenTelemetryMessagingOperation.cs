0cc1bcc18df61eed29d069758e31863011f08ddd
Tomas Izo <toizo@dfds.com>
2024-04-25 11:04:17 +0200

    remove unused operation

--- src/Dafda/Diagnostics/OpenTelemetryMessagingOperation.cs
+++ src/Dafda/Diagnostics/OpenTelemetryMessagingOperation.cs
@@ -5,11 +5,6 @@ internal static class OpenTelemetryMessagingOperation
     internal static class Producer
     {
         /// <summary>
-        /// A message is created or passed to a client library for publishing. “Create” spans always refer to a single message and are used to provide a unique creation context for messages in batch publishing scenarios.
-        /// </summary>
-        public const string Create = "create";
-
-        /// <summary>
         /// One or more messages are provided for publishing to an intermediary. If a single message is published, the context of the “Publish” span can be used as the creation context and no “Create” span needs to be created.
         /// </summary>
         public const string Publish = "publish";
