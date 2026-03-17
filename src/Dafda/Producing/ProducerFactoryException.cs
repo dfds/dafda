32e716a92e216d4b8c4036c7b3fd8ea48e95a280
Nabi Sobhi <seysobh@dfds.com>
2026-03-17 08:53:33 +0100

    revert unwanted

--- src/Dafda/Producing/ProducerFactoryException.cs
+++ src/Dafda/Producing/ProducerFactoryException.cs
@@ -5,7 +5,7 @@ namespace Dafda.Producing
 {
     /// <summary>
     /// Exception thrown when multiple identical producers are added via
-    /// <see cref="ProducerServiceCollectionExtensions.AddProducerFor{TClient}(Microsoft.Extensions.DependencyInjection.IServiceCollection,System.Action{Dafda.Configuration.ProducerOptions})"/>.
+    /// <see cref="ProducerServiceCollectionExtensions.AddProducerFor{TClient}"/>
     /// </summary>
     public sealed class ProducerFactoryException : Exception
     {
