7a88fdaa2f809b3e96fad5f431adc3a783148afe
Nabi Sobhi <seysobh@dfds.com>
2026-03-17 09:29:05 +0100

    fix doc

--- src/Dafda/Producing/ProducerFactoryException.cs
+++ src/Dafda/Producing/ProducerFactoryException.cs
@@ -4,8 +4,11 @@ using Dafda.Configuration;
 namespace Dafda.Producing
 {
     /// <summary>
-    /// Exception thrown when multiple identical producers are added via
-    /// <see cref="ProducerServiceCollectionExtensions.AddProducerFor{TClient}(Microsoft.Extensions.DependencyInjection.IServiceCollection,System.Action{Dafda.Configuration.ProducerOptions})"/>.
+    /// Exception thrown when multiple identical producers are added via any overload of
+    /// <see cref="ProducerServiceCollectionExtensions.AddProducerFor{TClient}(Microsoft.Extensions.DependencyInjection.IServiceCollection,System.Action{Dafda.Configuration.ProducerOptions})"/>,
+    /// <see cref="ProducerServiceCollectionExtensions.AddProducerFor{TService,TImplementation}(Microsoft.Extensions.DependencyInjection.IServiceCollection,System.Action{Dafda.Configuration.ProducerOptions})"/>,
+    /// <see cref="ProducerServiceCollectionExtensions.AddProducerFor{TClient}(Microsoft.Extensions.DependencyInjection.IServiceCollection,System.Func{System.IServiceProvider,Dafda.Configuration.ProducerOptions})"/>, or
+    /// <see cref="ProducerServiceCollectionExtensions.AddProducerFor{TService,TImplementation}(Microsoft.Extensions.DependencyInjection.IServiceCollection,System.Func{System.IServiceProvider,Dafda.Configuration.ProducerOptions})"/>.
     /// </summary>
     public sealed class ProducerFactoryException : Exception
     {
