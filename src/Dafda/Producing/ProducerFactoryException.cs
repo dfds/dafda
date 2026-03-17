6e34e8e2df04c461243ab5c43c101b31941be940
Nabi Sobhi <seysobh@dfds.com>
2026-03-17 08:35:02 +0100

    revert

--- src/Dafda/Producing/ProducerFactoryException.cs
+++ src/Dafda/Producing/ProducerFactoryException.cs
@@ -1,15 +1,16 @@
 using System;
 using Dafda.Configuration;
 
-namespace Dafda.Producing;
-
-/// <summary>
-/// Exception thrown when multiple identical producers are added via
-/// <see cref="ProducerServiceCollectionExtensions.AddProducerFor{TClient}(Microsoft.Extensions.DependencyInjection.IServiceCollection,System.Action{Dafda.Configuration.ProducerOptions})"/>.
-/// </summary>
-public sealed class ProducerFactoryException : Exception
+namespace Dafda.Producing
 {
-    internal ProducerFactoryException(string message) : base(message)
+    /// <summary>
+    /// Exception thrown when multiple identical producers are added via
+    /// <see cref="ProducerServiceCollectionExtensions.AddProducerFor{TClient}(Microsoft.Extensions.DependencyInjection.IServiceCollection,System.Action{Dafda.Configuration.ProducerOptions})"/>.
+    /// </summary>
+    public sealed class ProducerFactoryException : Exception
     {
+        internal ProducerFactoryException(string message) : base(message)
+        {
+        }
     }
 }
